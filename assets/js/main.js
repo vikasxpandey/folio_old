e291328c7e3b887c73b721e42f3a9541e4ac9af8
Vikas <vikas.pandey792@gmail.com>
2020-04-27 00:24:47 +0530

    skeleton-services

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
-$(() => {
-    AOS.init();
+$(function() {
+
+    // IE CHECK
+
+    var ua = window.navigator.userAgent;
+    var trident = ua.indexOf('Trident/');
+    var msie = ua.indexOf('MSIE ');
+
+    if (msie > 0 || trident > 0) {
+        $("[data-aos^=fade][data-aos^=fade]").removeAttr('data-aos');
+     } else {
+         AOS.init();
+     }
+
+    //  RELLAX
+
+    var rellax = new Rellax('.rellax');
+    
 });
